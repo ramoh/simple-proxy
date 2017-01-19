6b1c7cbca577f0b6beef88eca323ef7c4809af7e
ramoh <mohantyrajesh1984@gmail.com>
2017-01-19 21:37:30 +0530

    make it more better

--- proxy.js
+++ proxy.js
@@ -6,6 +6,6 @@ var request = require("request");
 http.createServer(function(req, res) {
   var options = url.parse(req.url, true);
   request(options.query.fpath).pipe(res);
-}).listen(8080);
+}).listen(process.env.PORT || 3080);
 
 console.log("proxy server started");
