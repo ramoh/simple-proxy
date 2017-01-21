8a8a30c53274ec3a074f603f48e03a4e1b5327c3
ramoh <mohantyrajesh1984@gmail.com>
2017-01-21 12:56:48 +0530

    adasd

--- proxy.js
+++ proxy.js
@@ -1,4 +1,3 @@
-/*
 var url = require('url');
 var http = require('http');
 var request = require("request");
@@ -10,31 +9,3 @@ http.createServer(function(req, res) {
 }).listen(process.env.PORT || 3080);
 
 console.log("proxy server started");
-*/
-
-
-
-var url = require('url');
-var http = require('http');
-var acceptor = http.createServer().listen(process.env.PORT || 3080);
-
-acceptor.on('request', function(request, response) {
-  console.log('request ' + request.url);
-  request.pause();
-  var options = url.parse(request.url, true);
-  options.headers = request.headers;
-  options.method = request.method;
-  options.agent = false;
-  options.hostname = options.query.fpath;
-
-  console.log("+++++options" + options);
-
-  var connector = http.request(options, function(serverResponse) {
-    serverResponse.pause();
-    response.writeHeader(serverResponse.statusCode, serverResponse.headers);
-    serverResponse.pipe(response);
-    serverResponse.resume();
-  });
-  request.pipe(connector);
-  request.resume();
-});
